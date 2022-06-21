e5671a57d62ce1bbcfec62ef324d0dd2999b4e9f
Corallus Caninus <41231332+Corallus-Caninus@users.noreply.github.com>
2022-06-20 17:21:43 -0700

    add: refactor into unittests for test driven development via function extraction

--- Brains.c
+++ Brains.c
@@ -3,14 +3,21 @@
 // NOTE: one session is one graph
 // NOTE: this library will add features as needed throughout my AI development
 
-// TODO: create an API for Neural Networks that is very C like but ergonomic
+// TODO: create an API for Neural Networks that is very C like but ergonomic with lazy builders
 #include <stdio.h>
 #include <tf_attrtype.h>
 
 #include "Model.c"
+#include "Brains.h"
+
+//PROTOTYPES
+void Test_MatMul(Model* model);
+
+void Test_Concat(Model* model);
 
 // TODO: void close_model(dealloc), tensor* infer(tensor*), tensor*
 // train(tensor*),
+
 void NoOpDeallocator(void* data, size_t a, void* b) {}
 
 // NOTE: see
@@ -18,13 +25,31 @@ void NoOpDeallocator(void* data, size_t a, void* b) {}
 // detailed example of all graph features in c api
 int main(int argc, char** argv) {
 	printf("Hello, World!\n");
+
 	// INIT SESSION//
 	Model* model = build();
-
-	// print the device currently running
 	printf("Device list output: %d\n",
 		TF_SessionListDevices(model->session, model->status));
 
+	//UNITTESTS
+	Test_Concat(model);
+	Test_MatMul(model);
+	//TODO: gradient test
+
+	//CLOSE SESSION//
+	delete_model(model);
+
+	printf("Goodbye, World!\n");
+	return 0;
+}
+
+void Test_Concat(Model* model)
+{
+	printf("\n\n");
+	printf("Test_Concat\n");
+	printf("\n");
+
+	printf("\n\n");
 	// BUILD GRAPH//
 	const int64_t dims[] = { 0 };
 	const int length = 1;
@@ -42,7 +67,7 @@ int main(int argc, char** argv) {
 	const int64_t dims3[] = { 0 };
 	const int length3 = 0;
 	TF_Operation* concat_dim_input_g =
-		add_op(model, "Const", "c_dim", dims3, length3, TF_INT32, NULL, 0, NULL,
+		add_tensor_op(model, "Const", "c_dim", dims3, length3, TF_INT32, NULL, 0, NULL,
 			0, sizeof(int));
 
 	//get the output edge for concat operation in the graph
@@ -55,19 +80,15 @@ int main(int argc, char** argv) {
 	inputs[1] = (TF_Output){ placeholder_op2_g, 0 };
 
 	TF_Operation* test_op_g =
-		add_op(model, "Concat", "c", dims, length, TF_FLOAT, concat_dim_input_out,
+		add_tensor_op(model, "Concat", "c", dims, length, TF_FLOAT, concat_dim_input_out,
 			1, inputs, 2, sizeof(float));
 
 	// DATA INPUT //
-	// TODO: prefer to map to an array in c with TF_AllocateTensor so we can
-	// update rapidly for sessions. then refactor into helper allocation init
-	// function and corresponding helper deallocation function (free and TF) SETUP
 	// INPUT TENSORS //
 	printf("Setting up input tensors\n");
 	int NumInputs = 2;
 	TF_Output* Inputs = malloc(sizeof(TF_Output) * NumInputs);
 	// insert placeholders into Inputs
-	// Inputs[0] = {placeholder_op_g, 0};
 	TF_Output first_input = { placeholder_op_g, 0 };
 	Inputs[0] = first_input;
 	TF_Output second_input = { placeholder_op2_g, 0 };
@@ -102,10 +123,6 @@ int main(int argc, char** argv) {
 	InputValues[1] = float_tensor2;
 
 	// call TF_SessionRun with all NULL
-	if (TF_GetCode(model->status) != TF_OK) {
-		fprintf(stderr, "TF_SessionRun error: %s\n", TF_Message(model->status));
-		return 1;
-	}
 	printf("TF_SessionRun success\n");
 
 	TF_Tensor** OutputValues =
@@ -130,12 +147,89 @@ int main(int argc, char** argv) {
 	printf("Verifying output\n");
 	printf("Output data: %f %f\n", output_data[0], output_data[1]);
 	printf("Last output data: %f %f\n", last_output_data[0], last_output_data[1]);
-
-	printf("Goodbye, World!\n");
 	//delete all the tensors we allocated
 	TF_DeleteTensor(float_tensor);
 	TF_DeleteTensor(float_tensor2);
 	TF_DeleteTensor(OutputValues[0]);
-	delete_model(model);
-	return 0;
+}
+
+void Test_MatMul(Model* model)
+{
+	printf("\n\n");
+	printf("Test_MatMul\n");
+	printf("\n");
+
+
+	//create an op for matrix multiply just like the above Concat code with two placeholder inputs
+	const int64_t dims4[] = { 2, 2 };
+	const int length4 = 2;
+	TF_Operation* placeholder_op_g2 =
+		placeholder(model, "c3", dims4, length4, TF_FLOAT);
+	const int64_t dims5[] = { 2, 2 };
+	const int length5 = 2;
+	TF_Operation* placeholder_op_g3 =
+		placeholder(model, "c4", dims5, length5, TF_FLOAT);
+
+	//create a matrix multiply op
+	TF_OperationDescription* test_op_g2 = TF_NewOperation(model->graph, "MatMul", "m");
+	TF_SetAttrType(test_op_g2, "T", TF_FLOAT);
+	TF_AddInput(test_op_g2, (TF_Output) { placeholder_op_g2, 0 });
+	TF_AddInput(test_op_g2, (TF_Output) { placeholder_op_g3, 0 });
+	TF_Operation* test_op_g3 = TF_FinishOperation(test_op_g2, model->status);
+	printf("Created matrix multiply op\n");
+	printTf(model);
+	//build the inputs and outputs for MatrixMult Operation and run it
+	//create input
+	printf("Creating input tensors for matmul\n");
+	int NumInputs_matmul = 2;
+	TF_Output* Inputs_matmul = malloc(sizeof(TF_Output) * NumInputs_matmul);
+	TF_Output first_input_matmul = { placeholder_op_g2, 0 };
+	Inputs_matmul[0] = first_input_matmul;
+	TF_Output second_input_matmul = { placeholder_op_g3, 0 };
+	Inputs_matmul[1] = second_input_matmul;
+	printf("Created input tensors for matmul\n");
+	printf("creating outputs for matmul\n");
+	int NumOutputs_matmul = 1;
+	TF_Output* Outputs_matmul = malloc(sizeof(TF_Output) * NumOutputs_matmul);
+	TF_Output first_output_matmul = { test_op_g3, 0 };
+	Outputs_matmul[0] = first_output_matmul;
+	printf("created outputs for matmul\n");
+	printf("adding data to input tensors\n");
+	TF_Tensor** InputValues_matmul = (TF_Tensor**)malloc(sizeof(TF_Tensor*) * NumInputs_matmul);
+	//generate data
+	float* data1_matmul = malloc(sizeof(float));
+	int64_t dims1_matmul[] = { 2, 2 };
+	TF_Tensor* float_tensor_matmul = new_tensor(TF_FLOAT, 2, dims1_matmul, data1_matmul, sizeof(float) * 4);
+	float* data1_ptr_matmul = (float*)TF_TensorData(float_tensor_matmul);
+	data1_ptr_matmul[0] = 1.0f;
+	data1_ptr_matmul[1] = 2.0f;
+	data1_ptr_matmul[2] = 3.0f;
+	data1_ptr_matmul[3] = 4.0f;
+	printf("Created input tensors for matmul\n");
+
+	float* data2_matmul = malloc(sizeof(float));
+	int64_t dims2_matmul[] = { 2, 2 };
+	TF_Tensor* float_tensor2_matmul = new_tensor(TF_FLOAT, 2, dims2_matmul, data2_matmul, sizeof(float) * 4);
+	float* data2_ptr_matmul = (float*)TF_TensorData(float_tensor2_matmul);
+	data2_ptr_matmul[0] = 3.0f;
+	data2_ptr_matmul[1] = 4.0f;
+	data2_ptr_matmul[2] = 5.0f;
+	data2_ptr_matmul[3] = 6.0f;
+
+	InputValues_matmul[0] = float_tensor_matmul;
+	InputValues_matmul[1] = float_tensor2_matmul;
+	printf("added data to input tensors\n");
+	//create output values
+	TF_Tensor** OutputValues_matmul =
+		(TF_Tensor**)malloc(sizeof(TF_Tensor*) * NumOutputs_matmul);
+
+	printf("running matmul\n");
+	TF_SessionRun(model->session, NULL, Inputs_matmul, InputValues_matmul, NumInputs_matmul, Outputs_matmul,
+		OutputValues_matmul, NumOutputs_matmul, 0, 0, NULL, model->status);
+	printf("ran matmul\n");
+	//print outputs
+	float* output_data_matmul = (float*)TF_TensorData(OutputValues_matmul[0]);
+	printf("Output data: %f %f\n", output_data_matmul[0], output_data_matmul[1]);
+	//print the other output
+	printf("Output data: %f %f\n", output_data_matmul[2], output_data_matmul[3]);
 }
--- Model.c
+++ Model.c
@@ -2,47 +2,47 @@
 
 // BEGIN CLASS//
 struct Model {
-  TF_Graph* graph;
-  TF_Session* session;
-  TF_SessionOptions* opts;
-  TF_Status* status;
-  TF_Output* inputs;
-  TF_Output* outputs;
-  // TODO: these must be done with AllocatTensor not NewTensor to ensure
-  //       we own the closest data pointer to GPU mapped memory which is
-  //       one of the primary goals of this framework.
-  TF_Tensor* input_tensor;
-  TF_Tensor* output_tensor;
+	TF_Graph* graph;
+	TF_Session* session;
+	TF_SessionOptions* opts;
+	TF_Status* status;
+	TF_Output* inputs;
+	TF_Output* outputs;
+	// TODO: these must be done with AllocatTensor not NewTensor to ensure
+	//       we own the closest data pointer to GPU mapped memory which is
+	//       one of the primary goals of this framework.
+	TF_Tensor* input_tensor;
+	TF_Tensor* output_tensor;
 };
 // do the above as a typedef
 typedef struct Model Model;
 static void printTf(Model* model) {
-  if (TF_GetCode(model->status) != TF_OK) {
-    printf("%s\n", TF_Message(model->status));
-  }
+	if (TF_GetCode(model->status) != TF_OK) {
+		printf("%s\n", TF_Message(model->status));
+	}
 }
 
 // TODO: want to return a struct that has function pointers that point to model
 // internally and subtype function pointer so we dont pass model between every
 // function (build your own C++ class)
 static Model* build() {
-  // initialize the graph etc.
-  TF_Graph* graph = TF_NewGraph();
-  TF_Status* status = TF_NewStatus();
-  const TF_SessionOptions* opts = TF_NewSessionOptions();
-  TF_Session* session = TF_NewSession(graph, opts, status);
-  Model* model = malloc(sizeof(Model));
-  model->graph = graph;
-  model->session = session;
-  model->opts = opts;
-  model->status = status;
-  printTf(model);
-  // TODO: implement
-  // model->inputs = malloc(sizeof(TF_Output));
-  // model->outputs = malloc(sizeof(TF_Output));
-  // model->input_tensor = malloc(sizeof(TF_Tensor));
-  // model->output_tensor = malloc(sizeof(TF_Tensor));
-  return model;
+	// initialize the graph etc.
+	TF_Graph* graph = TF_NewGraph();
+	TF_Status* status = TF_NewStatus();
+	const TF_SessionOptions* opts = TF_NewSessionOptions();
+	TF_Session* session = TF_NewSession(graph, opts, status);
+	Model* model = malloc(sizeof(Model));
+	model->graph = graph;
+	model->session = session;
+	model->opts = opts;
+	model->status = status;
+	printTf(model);
+	// TODO: implement
+	// model->inputs = malloc(sizeof(TF_Output));
+	// model->outputs = malloc(sizeof(TF_Output));
+	// model->input_tensor = malloc(sizeof(TF_Tensor));
+	// model->output_tensor = malloc(sizeof(TF_Tensor));
+	return model;
 }
 
 // TODO: find a way to subclass model so its stateful to the object using
@@ -75,44 +75,44 @@ static Model* build() {
 /// <param name="input_list_len"></param>
 ///  the length of the aforementioned list
 /// <returns></returns>
-static TF_Operation* add_op(Model* model, char* op_name_g, char* op_name,
-                            const int64_t* dims, int length, TF_DataType dtype,
-                            TF_Output* inputs, int num_inputs,
-                            TF_Output* input_list, int input_list_len,
-                            size_t type_size) {
-  // add op to graph
-  TF_OperationDescription* op =
-      TF_NewOperation(model->graph, op_name_g, op_name);
-  // add attr to op
-  TF_SetAttrType(op, "dtype", dtype);
-  printTf(model);
-  TF_Tensor* value = TF_AllocateTensor(dtype, dims, length, type_size);
-  // add shape
-  // TODO: is this redundant with the above?
-  TF_SetAttrShape(op, "shape", dims, length);
-  printTf(model);
+static TF_Operation* add_tensor_op(Model* model, char* op_name_g, char* op_name,
+	const int64_t* dims, int length, TF_DataType dtype,
+	TF_Output* inputs, int num_inputs,
+	TF_Output* input_list, int input_list_len,
+	size_t type_size) {
+	// add op to graph
+	TF_OperationDescription* op =
+		TF_NewOperation(model->graph, op_name_g, op_name);
+	// add attr to op
+	TF_SetAttrType(op, "dtype", dtype);
+	printTf(model);
+	TF_Tensor* value = TF_AllocateTensor(dtype, dims, length, type_size);
+	// add shape
+	//NOTE: for ops that dont have attributes these are ignored so these can be lazy NULL initialized builder
+	TF_SetAttrShape(op, "shape", dims, length);
+	printTf(model);
 
-  // set the tensor to have value 1
-  // TODO: does this only work because first value has no stride of TF_ length?
-  *(int*)TF_TensorData(value) = 0;
-  TF_SetAttrTensor(op, "value", value, model->status);
+	// set the tensor to have value 1
+	// TODO: does this only work because first value has no stride of TF_ length?
+	*(int*)TF_TensorData(value) = 0;
+	TF_SetAttrTensor(op, "value", value, model->status);
 
-  // add the inputs to the graph
-  for (int i = 0; i < num_inputs; i++) {
-    printf("inp");
-    TF_AddInput(op, inputs[i]);
-    printTf(model);
-  }
-  // TODO: how to handle multiple input lists for some ops? for now just build
-  // without helper function
-  //  add the input_list
-  if (input_list) TF_AddInputList(op, input_list, input_list_len);
-  printTf(model);
+	// add the inputs to the graph
+	for (int i = 0; i < num_inputs; i++) {
+		printf("inp");
+		TF_AddInput(op, inputs[i]);
+		printTf(model);
+	}
+	// TODO: how to handle multiple input lists for some ops? for now just build
+	// without helper function
+	//  add the input_list
+	if (input_list) TF_AddInputList(op, input_list, input_list_len);
+	printTf(model);
 
-  //  finish the op
-  TF_Operation* operation = TF_FinishOperation(op, model->status);
-  printTf(model);
-  return operation;
+	//  finish the op
+	TF_Operation* operation = TF_FinishOperation(op, model->status);
+	printTf(model);
+	return operation;
 }
 
 // TODO: an entire malloc -> input placeholder abstraction helper function
@@ -127,18 +127,18 @@ static TF_Operation* add_op(Model* model, char* op_name_g, char* op_name,
 /// <param name="dtype"></param>
 /// <returns></returns>
 static TF_Operation* placeholder(Model* model, char* op_name,
-                                 const int64_t* dims, int length,
-                                 TF_DataType dtype) {
-  TF_OperationDescription* op =
-      TF_NewOperation(model->graph, "Placeholder", op_name);
-  printTf(model);
-  TF_SetAttrType(op, "dtype", dtype);
-  printTf(model);
-  TF_SetAttrShape(op, "shape", dims, length);
-  printTf(model);
-  TF_Operation* operation = TF_FinishOperation(op, model->status);
-  printTf(model);
-  return operation;
+	const int64_t* dims, int length,
+	TF_DataType dtype) {
+	TF_OperationDescription* op =
+		TF_NewOperation(model->graph, "Placeholder", op_name);
+	printTf(model);
+	TF_SetAttrType(op, "dtype", dtype);
+	printTf(model);
+	TF_SetAttrShape(op, "shape", dims, length);
+	printTf(model);
+	TF_Operation* operation = TF_FinishOperation(op, model->status);
+	printTf(model);
+	return operation;
 }
 
 // TODO: new_Constant //return a constant to be passed into an op
@@ -153,9 +153,9 @@ static TF_Operation* placeholder(Model* model, char* op_name,
 /// <param name="graph_op_name"></param>
 /// <param name="input"></param>
 static void add_op_input(Model* model, char* graph_op_name, TF_Output input) {
-  // add input to op
-  TF_Operation* op = TF_GraphOperationByName(model->graph, graph_op_name);
-  TF_AddInput(op, input);
+	// add input to op
+	TF_Operation* op = TF_GraphOperationByName(model->graph, graph_op_name);
+	TF_AddInput(op, input);
 }
 // same as above but for input_list
 /// <summary>
@@ -167,10 +167,10 @@ static void add_op_input(Model* model, char* graph_op_name, TF_Output input) {
 /// <param name="inputs"></param>
 /// <param name="num_inputs"></param>
 static void add_op_input_list(Model* model, char* graph_op_name,
-                              TF_Input* inputs, int num_inputs) {
-  // add input list to op
-  TF_Operation* op = TF_GraphOperationByName(model->graph, graph_op_name);
-  TF_AddInputList(op, inputs, num_inputs);
+	TF_Input* inputs, int num_inputs) {
+	// add input list to op
+	TF_Operation* op = TF_GraphOperationByName(model->graph, graph_op_name);
+	TF_AddInputList(op, inputs, num_inputs);
 }
 /// <summary>
 /// Retrieves the Operation from the graph by name and inserts the given input.
@@ -180,10 +180,10 @@ static void add_op_input_list(Model* model, char* graph_op_name,
 /// <param name="index"></param>
 /// <returns></returns>
 static TF_Output get_op_output(Model* model, char* graph_op_name, int index) {
-  // get output from op
-  TF_Operation* op = TF_GraphOperationByName(model->graph, graph_op_name);
-  TF_Output res = {op, index};
-  return res;
+	// get output from op
+	TF_Operation* op = TF_GraphOperationByName(model->graph, graph_op_name);
+	TF_Output res = { op, index };
+	return res;
 }
 /// <summary>
 /// Retrieves the Operation from the graph by name and returns the output list.
@@ -194,15 +194,15 @@ static TF_Output get_op_output(Model* model, char* graph_op_name, int index) {
 /// <param name="end_index"></param>
 /// <returns></returns>
 static TF_Output* get_op_output_list(Model* model, char* graph_op_name,
-                                     int start_index, int end_index) {
-  // get output from op
-  TF_Operation* op = TF_GraphOperationByName(model->graph, graph_op_name);
-  TF_Output* res = malloc(sizeof(TF_Output) * (end_index - start_index + 1));
-  for (int i = start_index; i <= end_index; i++) {
-    TF_Output cur_output = {op, i};
-    res[i - start_index] = cur_output;
-  }
-  return res;
+	int start_index, int end_index) {
+	// get output from op
+	TF_Operation* op = TF_GraphOperationByName(model->graph, graph_op_name);
+	TF_Output* res = malloc(sizeof(TF_Output) * (end_index - start_index + 1));
+	for (int i = start_index; i <= end_index; i++) {
+		TF_Output cur_output = { op, i };
+		res[i - start_index] = cur_output;
+	}
+	return res;
 }
 //create a tensor and return it
 //essentially we are making a function extraction of the following:
@@ -230,14 +230,14 @@ static TF_Tensor* new_tensor(TF_DataType dtype, int ndims, int64_t* dims, float*
 /// </summary>
 /// <param name="model"></param>
 static void delete_model(Model* model) {
-  // delete model
-  TF_DeleteStatus(model->status);
-  TF_DeleteSession(model->session, model->status);
-  TF_DeleteGraph(model->graph);
-  // TODO: these point to graph worst case this is double-free
-  if (model->inputs) free(model->inputs);
-  if (model->outputs) free(model->outputs);
-  TF_DeleteTensor(model->input_tensor);
-  TF_DeleteTensor(model->output_tensor);
+	// delete model
+	TF_DeleteStatus(model->status);
+	TF_DeleteSession(model->session, model->status);
+	TF_DeleteGraph(model->graph);
+	// TODO: these point to graph worst case this is double-free
+	if (model->inputs) free(model->inputs);
+	if (model->outputs) free(model->outputs);
+	TF_DeleteTensor(model->input_tensor);
+	TF_DeleteTensor(model->output_tensor);
 }
 // END CLASS//
